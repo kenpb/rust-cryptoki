1e7a264729f9265c3c318c160c7235004504421e
kenpb <kpalmab@icloud.com>
2021-04-22 22:37:38 +0000

    Add x64 Windows bindings

--- cryptoki-sys/src/lib.rs
+++ cryptoki-sys/src/lib.rs
@@ -60,6 +60,16 @@ include!(concat!(
     "/src/bindings/arm-unknown-linux-gnueabi.rs"
 ));
 
+#[cfg(all(
+  not(feature = "generate-bindings"),
+  target_arch = "x86_64",
+  target_os = "windows"
+))]
+include!(concat!(
+  env!("CARGO_MANIFEST_DIR"),
+  "/src/bindings/x86_64-pc-windows-msvc.rs"
+));
+
 // If the "generate-bindings" feature is on, use the generated bindings.
 #[cfg(feature = "generate-bindings")]
 include!(concat!(env!("OUT_DIR"), "/pkcs11_bindings.rs"));
